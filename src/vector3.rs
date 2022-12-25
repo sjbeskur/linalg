a7e436f858081602f2c377a5dcf6bcefbb720018
sjbeskur <sam.beskur@gmail.com>
2022-12-24 18:19:21 -0700

    vector3 mostly working

---
+++ src/vector3.rs
@@ -0,0 +1,106 @@
+#![allow(dead_code)]
+use std::ops;
+
+#[derive(Debug)]
+pub struct Vector3{
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector3 {
+
+    pub fn new(x: f32, y: f32, z: f32) -> Vector3{
+        Self{ x,y,z }
+    }
+
+    pub fn zeros() -> Vector3{
+        Self{ x: 0.0, y: 0.0, z: 0.0 }
+    }
+
+    /// The dot product between two vectors (also known as the inner product) returns
+    /// a number and is expressed using the "dot(..)" function.  To be clear the return value
+    /// is not a vector but a single scalar value.
+    /// 
+    /// Algebraically it is represented between brackets like so: (V, W) where V and W are 
+    /// Vector3. 
+    /// One method to calculate the dot product is:
+    /// dotprod = (Vx * Wx + Vy * Wy + Vz * Wz);      
+    /// 
+    /// 
+    pub fn dot(&self, other: &Vector3) -> f32 {
+        self.x * other.x 
+      + self.y * other.y 
+      + self.z * other.z
+    }
+
+    /// Same as 'magnitude' of a vector.
+    /// It is equal to the square root of the vectors dot product with itself
+    pub fn length(&self) -> f32 {
+        f32::sqrt(self.dot(self))
+    }
+
+    /// Same as 'length' of a vector.
+    /// It is equal to the square root of the vectors dot product with itself
+    pub fn mag(&self) -> f32 {
+        self.length()
+    }
+
+    pub fn cross(&self, other: &Vector3) -> Vector3 {
+        Self{
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }   
+    }
+
+    pub fn angle_between_r(&self, other: &Vector3) -> f32 {
+        let mag1  = self.mag();
+        let mag2 = other.mag();
+        let r = self.dot(other) / (mag1 * mag2);
+        f32::acos(r)
+    }
+
+    pub fn angle_between_d(&self, other: &Vector3) -> f32 {
+        self.angle_between_r(other) * 180.0 / std::f32::consts::PI
+    }
+
+}
+
+
+impl ops::Add for Vector3{
+    type Output = Vector3;
+    fn add(self, rhs: Vector3)-> Vector3{
+        Self{
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }                
+    }
+}
+
+impl ops::Sub for Vector3{
+    type Output = Vector3;
+    fn sub(self, rhs: Vector3)-> Vector3{
+        Self{
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }                
+    }
+}
+
+impl PartialEq for Vector3{
+    fn eq(&self, other: &Vector3) -> bool{
+        self.x == other.x &&
+        self.y == other.y &&
+        self.z == other.z
+    }
+}
+
+
+impl std::fmt::Display for Vector3{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f,"[x: {}, y: {}, z: {}]", self.x, self.y, self.z )
+    }    
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,81 @@
+
+use linalg::vector3::*;
+
+#[test]
+fn test_new_vector() { 
+    let vector = Vector3::new(3.0, 4.0, 0.0);
+    assert_eq!(vector.x, 3.0);
+    assert_eq!(vector.y, 4.0);
+    assert_eq!(vector.z, 0.0);
+}    
+
+#[test]
+fn test_dot_product() { 
+    let v1 = Vector3::new(3.0, 4.0, 0.0);
+    let v2 = Vector3::new(3.0, 4.0, 0.0);
+    println!("dot {}", v1.dot(&v2));
+    assert_eq!(v1.dot(&v2), 25.0);
+    assert_eq!(v2.dot(&v1), 25.0);
+
+    let v3 = Vector3::new(-6.0, 8.0, 0.0);
+    let v4 = Vector3::new(5.0, 12.0, 0.0);
+    //println!("dot {:.3}", v3.dot(&v4));
+    assert_eq!(v3.dot(&v4), 66.0);
+    assert_eq!(v4.dot(&v3), 66.0);
+}    
+
+#[test]
+fn test_length(){
+    let v1 = Vector3::new(3.0, 4.0, 0.0);
+    assert_eq!(v1.length(), 5.0);
+
+    let v3 = Vector3::new(-6.0, 8.0, 0.0);
+    let v4 = Vector3::new(5.0, 12.0, 0.0);
+    assert_eq!(v3.length(), 10.0);
+    assert_eq!(v4.length(), 13.0);
+
+    let v5 = Vector3::new(-6.6, 8.834, 0.0);
+    assert_eq!(v5.mag(), 11.027219);
+}
+
+#[test]
+fn test_cross_product() {
+    let v1 = Vector3::new(2.0, 3.0, 4.0);
+    let v2 = Vector3::new(5.0, 6.0, 7.0);
+    let v3 = v1.cross(&v2);
+    assert_eq!(v3.x, -3.0);
+    assert_eq!(v3.y, 6.0);
+    assert_eq!(v3.z, -3.0);
+}
+
+#[test]
+fn test_angle_between(){
+    let v1 = Vector3::new(-4.0,4.0,0.0);
+    let v2 = Vector3::new(-2.0,-5.0,4.0);
+
+    //assert_eq!(0.0, v1.angle_between_r(&origin));
+    assert_eq!(1.8925469, v1.angle_between_r(&v2));
+    assert_eq!(108.43495, v1.angle_between_d(&v2));
+    assert_eq!(1.8925469, v2.angle_between_r(&v1));
+    assert_eq!(108.43495, v2.angle_between_d(&v1));
+
+    let v3 = Vector3::new(50.0,50.0,0.0);
+    let v4 = Vector3::new(0.0,50.0,0.0);
+    assert_eq!(45.0,v4.angle_between_d(&v3));
+    let v5 = Vector3::new(50.0,0.0,0.0);
+    assert_eq!(90.0, v4.angle_between_d(&v5));
+
+    let v6 = Vector3::new(0.0,-50.0,0.0);
+    assert_eq!(180.0, v4.angle_between_d(&v6));
+}
+
+#[test]
+fn test_equality(){
+    let v6 = Vector3::new(0.0,-50.0,0.0);
+    assert!(&v6 == &v6);
+    assert_eq!(v6 , v6);
+    let origin = Vector3::zeros();
+    let zeros = Vector3::new(0.0,0.0,0.0);
+    assert_eq!(&origin,&zeros);
+}
+
